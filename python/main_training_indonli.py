770f1c54d1550435c56ce705b1e977b7ef0ed62f
Ravi <muhammadravi251001@gmail.com>
2023-02-25 13:24:47 +0700

    modifying help function again

--- python/main_training_indonli.py
+++ python/main_training_indonli.py
@@ -6,9 +6,9 @@ parser.add_argument('-m', '--model_name', type=str, metavar='', required=True, h
 parser.add_argument('-d', '--data_name', type=str, metavar='', required=True, help="Nama dataset Anda; String")
 parser.add_argument('-e', '--epoch', type=int, metavar='', required=True, help="Jumlah epoch Anda; Integer")
 parser.add_argument('-sa', '--sample', type=str, metavar='', required=True, help="Jumlah sampling data Anda; Integer (namun bisa juga \"max\")")
-parser.add_argument('-l', '--learn_rate', type=float, metavar='', required=False, help="Jumlah learning rate Anda; Float", default=1e-5)
-parser.add_argument('-se', '--seed', type=int, metavar='', required=False, help="Jumlah seed Anda; Integer", default=42)
-parser.add_argument('-t', '--token', type=str, metavar='', required=False, help="Token Hugging Face Anda; String", default="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")
+parser.add_argument('-l', '--learn_rate', type=float, metavar='', required=False, help="Jumlah learning rate Anda; Float; default=1e-5", default=1e-5)
+parser.add_argument('-se', '--seed', type=int, metavar='', required=False, help="Jumlah seed Anda; Integer; default=42", default=42)
+parser.add_argument('-t', '--token', type=str, metavar='', required=False, help="Token Hugging Face Anda; String; default=(TOKEN_HF_RAVI)", default="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")
 args = parser.parse_args()
 
 if __name__ == "__main__":
