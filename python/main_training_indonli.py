702f6b6a1f27df9839fc2ad759f7a90680b46f2a
Ravi <muhammadravi251001@gmail.com>
2023-02-25 13:29:22 +0700

    modifying argparse helper

--- python/main_training_indonli.py
+++ python/main_training_indonli.py
@@ -2,13 +2,13 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser(description="Program untuk training IndoNLI")
-parser.add_argument('-m', '--model_name', type=str, metavar='', required=True, help="Nama model Anda; String")
-parser.add_argument('-d', '--data_name', type=str, metavar='', required=True, help="Nama dataset Anda; String")
-parser.add_argument('-e', '--epoch', type=int, metavar='', required=True, help="Jumlah epoch Anda; Integer")
-parser.add_argument('-sa', '--sample', type=str, metavar='', required=True, help="Jumlah sampling data Anda; Integer (namun bisa juga \"max\")")
-parser.add_argument('-l', '--learn_rate', type=float, metavar='', required=False, help="Jumlah learning rate Anda; Float; default=1e-5", default=1e-5)
-parser.add_argument('-se', '--seed', type=int, metavar='', required=False, help="Jumlah seed Anda; Integer; default=42", default=42)
-parser.add_argument('-t', '--token', type=str, metavar='', required=False, help="Token Hugging Face Anda; String; default=(TOKEN_HF_RAVI)", default="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")
+parser.add_argument('-m', '--model_name', type=str, metavar='', required=True, help="Nama model Anda; String; choice=[indolem, indonlu, xlmr]")
+parser.add_argument('-d', '--data_name', type=str, metavar='', required=True, help="Nama dataset Anda; String; choice=[basic, translated, augmented]")
+parser.add_argument('-e', '--epoch', type=int, metavar='', required=True, help="Jumlah epoch Anda; Integer; choice=[all integer]")
+parser.add_argument('-sa', '--sample', type=str, metavar='', required=True, help="Jumlah sampling data Anda; Integer; choice=[max, all integer]")
+parser.add_argument('-l', '--learn_rate', type=float, metavar='', required=False, help="Jumlah learning rate Anda; Float; choice=[all float]; default=1e-5", default=1e-5)
+parser.add_argument('-se', '--seed', type=int, metavar='', required=False, help="Jumlah seed Anda; Integer; choice=[all integer]; default=42", default=42)
+parser.add_argument('-t', '--token', type=str, metavar='', required=False, help="Token Hugging Face Anda; String; choice=[all string token]; default=(TOKEN_HF_muhammadravi251001)", default="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")
 args = parser.parse_args()
 
 if __name__ == "__main__":
